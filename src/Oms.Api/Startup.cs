3feec7ca062759d67ca602e4b819219de39bc89d
Luka <sabo.luka@gmail.com>
2020-10-20 10:31:45 +0200

    remove weather

--- src/Oms.Api/Startup.cs
+++ src/Oms.Api/Startup.cs
@@ -1,20 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Oms.Api.Extensions;
 using Oms.Domain.Extensions;
 using Oms.Domain.Repositories;
-using Oms.Infrastructure;
 using Oms.Infrastructure.Repositories;
 
 namespace Oms.Api
@@ -31,24 +22,14 @@ namespace Oms.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services
-            //    .AddEntityFrameworkSqlServer()
-            //    .AddOmsContext(Configuration.GetSection("DataSource:ConnectionString").Value)
-            //    .AddMappers()
-            //    .AddServices()
-            //    .AddControllers()
-            //    .AddValidation();
             services
                 .AddOmsContext(Configuration.GetSection("DataSource:ConnectionString").Value)
                 .AddScoped<ICmRepository, CmRepository>()
-                //.AddScoped<IOrderProductRepository, OrderRepository>()
-                //.AddScoped<IGenreRepository, GenreRepository>()
                 .AddMappers()
                 .AddServices()
                 .AddControllers()
                 .AddValidation()
                 .AddJsonOptions(options => options.JsonSerializerOptions.IgnoreNullValues = true);
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
